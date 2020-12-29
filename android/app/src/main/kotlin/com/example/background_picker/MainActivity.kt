09dda3af5e6fb9256542a69135319d04d137bde4
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-29 12:29:14 +0530

    FLUT-3278 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/background_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.background_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(Background_Image());
+
+class Background_Image extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: SafeArea(
+          child: Card(
+            margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+            child: Container(
+                constraints: BoxConstraints.expand(),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("icons/image.png"),
+                        fit: BoxFit.cover)),
+                child: SfDateRangePicker(
+                  headerStyle: DateRangePickerHeaderStyle(
+                      textStyle: TextStyle(color: Colors.white)),
+                  monthViewSettings: DateRangePickerMonthViewSettings(
+                      viewHeaderStyle: DateRangePickerViewHeaderStyle(
+                          textStyle: TextStyle(color: Colors.white))),
+                  backgroundColor: Colors.transparent,
+                  monthCellStyle: DateRangePickerMonthCellStyle(
+                      textStyle: TextStyle(color: Colors.white)),
+                )),
+          ),
+        )));
+  }
+}
