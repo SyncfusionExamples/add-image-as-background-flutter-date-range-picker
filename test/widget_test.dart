3bf0c7151c660e732e76c6283a9f8160c5630d35
Sasikumar Arumugam <sasikumar.arumugam@syncfusion.com>
2021-04-08 16:19:20 +0530

    FLUT-4313-Sample updated with null safety.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:background_picker/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Background_Image());
+    await tester.pumpWidget(BackgroundImagePicker());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
