c5dccd532ef434efbb99b218be2e917e9fdc356e
Muniappan <muniappan.subramanian@syncfusiopn.com>
2023-03-29 18:00:12 +0530

    sample updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
-void main() => runApp(BackgroundImagePicker());
+void main() => runApp(const BackgroundImagePicker());
 
 class BackgroundImagePicker extends StatelessWidget {
+  const BackgroundImagePicker({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
             body: SafeArea(
-          child: Card(
-            margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
-            child: Container(
-                constraints: BoxConstraints.expand(),
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("icons/image.png"),
-                        fit: BoxFit.cover)),
-                child: SfDateRangePicker(
-                  headerStyle: DateRangePickerHeaderStyle(
-                      textStyle: TextStyle(color: Colors.white)),
-                  monthViewSettings: DateRangePickerMonthViewSettings(
-                      viewHeaderStyle: DateRangePickerViewHeaderStyle(
-                          textStyle: TextStyle(color: Colors.white))),
-                  backgroundColor: Colors.transparent,
-                  monthCellStyle: DateRangePickerMonthCellStyle(
-                      textStyle: TextStyle(color: Colors.white)),
-                  yearCellStyle: DateRangePickerYearCellStyle(
-                      textStyle: TextStyle(color: Colors.white),
-                      leadingDatesTextStyle: TextStyle(color: Colors.white),
-                      disabledDatesTextStyle: TextStyle(color: Colors.white)),
-                )),
-          ),
-        )));
+              child: Card(
+                margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+                child: Container(
+                    constraints: const BoxConstraints.expand(),
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("icons/image.png"),
+                            fit: BoxFit.cover)),
+                    child: SfDateRangePicker(
+                      headerStyle: const DateRangePickerHeaderStyle(
+                          textStyle: TextStyle(color: Colors.white)),
+                      monthViewSettings: const DateRangePickerMonthViewSettings(
+                          viewHeaderStyle: DateRangePickerViewHeaderStyle(
+                              textStyle: TextStyle(color: Colors.white))),
+                      backgroundColor: Colors.transparent,
+                      monthCellStyle: const DateRangePickerMonthCellStyle(
+                          textStyle: TextStyle(color: Colors.white)),
+                      yearCellStyle: const DateRangePickerYearCellStyle(
+                          textStyle: TextStyle(color: Colors.white),
+                          leadingDatesTextStyle: TextStyle(color: Colors.white),
+                          disabledDatesTextStyle: TextStyle(color: Colors.white)),
+                    )),
+              ),
+            )));
   }
 }
